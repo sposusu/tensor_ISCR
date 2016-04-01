698edc9af1f8311b8004e4995296d856ba4b8666
Antonie Lin <iammrhelo@gmail.com>
2016-04-01 14:53:38 +0800

    Changed framework from bottle to flask

--- src/server.py
+++ src/server.py
@@ -1,48 +1,46 @@
-import argparse
-import logging
-import json
-import sys
-
-from bottle import route, view, get, post, run
-from bottle import request, response, HTTPResponse
-from bottle import redirect
-
-import api
-from IR.actionmanager import genActionTable
-from IR.dialoguemanager import DialogueManager
 """
 
   This is our Interactive Retrieval System's server
 
 """
+import argparse
+import logging
+import json
+import sys
+
+from flask import Flask
+from flask import request, abort
+from flask import render_template, jsonify
+
+import api
+from IR.actionmanager import genActionTable
+
+app = Flask(__name__,template_folder='views')
+
 ActionTable = genActionTable()
-
-
 dialoguemanager = api.get_dialoguemanger()
-agent = api.get_agent()
-
-agent.start_testing()
+#agent = api.get_agent()
+#agent.start_testing()
 
 # Index Page
-@route('/')
-@view('index')
+@app.route('/')
 def index():
-  return {}
+  return render_template('index.html')
 
 # Interact with Simulators
-@post('/interact')
+@app.route('/interact',methods=['POST'])
 def interact():
-  action = request.params.get('action','')
+  action = request.form.get('action','')
 
   if action == 'firstpass':
     # Sets session for current dialogue manager
-    query  = request.params.get('query','')
+    query  = request.form.get('query','')
     query = toint(json.loads(query))
 
-    ans = request.params.get('ans','')
+    ans = request.form.get('ans','')
     ans = toint(json.loads(ans))
 
-    ans_index = request.params.get('ans_index','')
+    ans_index = request.form.get('ans_index','')
 
     assert check_int_dict(query) and check_int_dict(ans)
 
@@ -50,15 +48,15 @@ def interact():
     dialoguemanager.expand_query({'action':'firstpass','query':query})
 
   elif action in ['doc','topic','keyterm','request']:
-    feedback_data = dict(request.params.items())
+    feedback_data = dict(request.form.items())
     dialoguemanager.expand_query(feedback_data)
 
   else:
-    return HTTPResponse(status_code=404, body='Action {0} not found!'.format(action))
+    abort(404)
+    #return HTTPResponse(status_code=404, body='Action {0} not found!'.format(action))
 
   # Generate State for Agent
   state = dialoguemanager.gen_state_feature()
-
   # Agent Part
   if action == 'firstpass':
     action_type = agent.start_episode(state)
@@ -66,15 +64,10 @@ def interact():
     reward = dialoguemanager.calculate_reward()
     action_type = agent.step(reward,state)
 
-  # Set response params
-  params = dialoguemanager.request(action_type)
+  # Set response form
+  form = dialoguemanager.request(action_type)
 
-  return params
-
-# Monitor
-@get('/admin/<command>')
-def admin(command):
-  return 'Admin'
+  return jsonify(**form)
 
 def toint(d):
   return dict( [ (int(k),float(v)) for k,v in d.iteritems() ] )
@@ -86,7 +79,7 @@ def main():
   parser.add_argument('-p','--port',type=int,default=1111)
   args = parser.parse_args()
 
-  run(host='localhost', port=1111, debug=True, reloader=True)
+  app.run(host='localhost', port=1111, debug=True)
 
   return 0
 
