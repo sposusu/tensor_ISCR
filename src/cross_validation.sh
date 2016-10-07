bf4bad110260e3a5c6795f831e79565144554d4d
Antonie Lin <iammrhelo@gmail.com>
2016-10-07 17:12:34 +0800

    delete old scripts

--- src/cross_validation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-  python DeepReinforce.py -t $1 -f ${i} -nsu --feature $2 --prefix new_simulated_user_$1_$2 &
-done
--- src/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-  python DeepReinforce.py -t $1 -f ${i} --feature $2 --prefix $3 &
-done
