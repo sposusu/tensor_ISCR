4939cc6568802cee57efe43c4c74c1a866b49538
Antonie Lin <iammrhelo@gmail.com>
2016-10-08 11:57:39 +0800

    Cross Validation Script

---
+++ src/cross_validation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# -f : fold
+# --feature: all/raw/wig/nqc
+# --directory: data directory $ISCR_HOME/data/target_directory/
+# --result: result directory $ISCR_HOME/result/
+# --name: experiment_name
+
+for i in {1..10}
+do
+    python run.py -f ${i} --feature ${1} --directory ${2} --result ${3} --name ${4}
+done
