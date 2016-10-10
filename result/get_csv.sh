8f378f6a602549e7c46b486daaa49b721d7010c9
Antonie Lin <iammrhelo@gmail.com>
2016-10-10 17:30:17 +0800

    remove unused result bash scripts

--- result/get_csv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-cat $1 |grep INFO | sed 's/INFO:root://g' | grep MAP | sed 's/MAP = //g' | sed 's/Return = //g' | sed 's/\t/,/g'
--- result/get_csvs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-  ./get_csv.sh $1${i}.log > $1${i}.csv
-done
-
